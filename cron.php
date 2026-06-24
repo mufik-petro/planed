f69ac1c3fe882576a1cbd3b3853fec85105b9dd0
mufik-petro <75790712+mufik-petro@users.noreply.github.com>
2026-06-24 18:01:32 +0300

    Update cron.php

--- cron.php
+++ cron.php
@@ -9,11 +9,27 @@ $log["list"] = $getList;
 
 if ($getList != NULL && is_array($getList)) {
     foreach ($getList as $key => $oneList) {
-        $fire["name"] = $oneList["trigger"];
-        $action = json_decode(send_bearer("https://api.smartsender.com/v1/contacts/".$oneList["userId"]."/fire", $oneList["token"], "POST", $fire), true);
-        $log["data"][$key]["fire"] = $fire;
-        $log["data"][$key]["result"] = $action;
-        if ($action["state"] == true) {
+        if ($oneList["trigger"] != NULL) {
+            $fire["name"] = $oneList["trigger"];
+            $action = json_decode(send_bearer("https://api.smartsender.com/v1/contacts/".$oneList["userId"]."/fire", $oneList["token"], "POST", $fire), true);
+            $log["data"][$key]["fire"] = $fire;
+            $log["data"][$key]["result"] = $action;
+            if ($action["state"] == true) {
+                mysqli_query($sqlConnect, "DELETE FROM `".$table."` WHERE `id` = '".$oneList["id"]."'");
+            }
+        } else if ($oneList["message"] != NULL) {
+            $message = [
+                "type" => "text",
+                "watermark" => 1,
+                "content" => $oneList["message"]
+            ];
+            $action = json_decode(send_bearer("https://api.smartsender.com/v1/contacts/".$oneList["userId"]."/send", $oneList["token"], "POST", $message), true);
+            $log["data"][$key]["fire"] = $fire;
+            $log["data"][$key]["result"] = $action;
+            if ($action["state"] == true) {
+                mysqli_query($sqlConnect, "DELETE FROM `".$table."` WHERE `id` = '".$oneList["id"]."'");
+            }
+        } else {
             mysqli_query($sqlConnect, "DELETE FROM `".$table."` WHERE `id` = '".$oneList["id"]."'");
         }
     }
